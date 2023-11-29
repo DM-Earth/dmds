c9fa87a9b6ffd55443769e6831d006365f240acf
Yjn024 <jiening.yu@outlook.com>
2023-11-29 22:06:19 +0800

    ignore vscode

--- tokio-daemon/src/lib.rs
+++ tokio-daemon/src/lib.rs
@@ -1,16 +1,21 @@
 use std::{path::PathBuf, pin::Pin};
 
 use async_trait::async_trait;
+use bytes::BytesMut;
 use dashmap::DashSet;
 use dmds::IoHandle;
 use tokio::{fs::File, io::BufReader};
 
 #[derive(Debug)]
 pub struct FsHandle {
-    path: PathBuf,
+    /// Root path of the database location.
+    root: PathBuf,
+
+    /// Whether the file path should be flat.
     flat: bool,
 
-    invalid_paths: DashSet<PathBuf>,
+    /// Set of chunk positions that is not available.
+    invalid_chunks: DashSet<Box<[usize]>>,
 }
 
 #[async_trait]
@@ -21,20 +26,22 @@ impl IoHandle for FsHandle {
         &self,
         pos: [usize; DIMS],
     ) -> std::io::Result<Self::Read<'_>> {
-        let path = self.path(&pos);
-
-        if self.invalid_paths.contains(&path) {
+        if self.invalid_chunks.contains(&pos[..]) {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::NotFound,
                 "requested file not found",
             ));
         }
 
+        let path = self.path(&pos);
         let result = File::open(&path).await;
 
         if let Err(ref err) = result {
             if err.kind() == std::io::ErrorKind::NotFound {
-                self.invalid_paths.insert(path);
+                if self.invalid_chunks.len() > Self::IP_CAPACITY {
+                    self.invalid_chunks.clear();
+                }
+                self.invalid_chunks.insert(Box::new(pos));
             }
         }
 
@@ -46,12 +53,24 @@ impl IoHandle for FsHandle {
 }
 
 impl FsHandle {
+    const IP_CAPACITY: usize = 1024 * 10;
+
+    /// Write a chunk to the file system.
     pub async fn write_chunk<const DIMS: usize, T: dmds::Data>(
         &self,
         chunk: &dmds::Chunk<T, DIMS>,
     ) -> std::io::Result<()> {
+        let mut buf = BytesMut::new();
+        chunk.write_buf(&mut buf).await?;
+        let buf = buf.freeze();
+
         let path = self.path(chunk.pos());
-        todo!()
+        let mut file = File::options().write(true).append(false).open(path).await?;
+        tokio::io::AsyncWriteExt::write_all(&mut file, &buf).await?;
+        tokio::io::AsyncWriteExt::flush(&mut file).await?;
+        file.sync_all().await?;
+        self.invalid_chunks.remove(&chunk.pos()[..]);
+        Ok(())
     }
 
     fn path(&self, pos: &[usize]) -> PathBuf {
@@ -63,11 +82,11 @@ impl FsHandle {
             }
             chunk.pop();
 
-            let mut buf = self.path.to_owned();
+            let mut buf = self.root.to_owned();
             buf.push(chunk);
             buf
         } else {
-            let mut buf = self.path.to_owned();
+            let mut buf = self.root.to_owned();
             for dim in pos {
                 buf.push(dim.to_string());
             }
